6ab8315710b85045391bee4f3544c01be11ac497
unknown <shukumar-542>
2022-03-24 10:42:19 +0600

    miniblog done

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,HttpResponseRedirect
-from .forms import userForm,LoginForm
+from .forms import userForm,LoginForm,postForm
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from .models import Post
+from django.contrib.auth.models import Group
 
 # HOME
 def home(request):
@@ -48,28 +49,50 @@ def signup(request):
       if request.method == 'POST':
             form = userForm(request.POST)
             if form.is_valid():
-                  # messages.success(request,'You Account Created SuccessFully')
-                  form.save()
+                  messages.success(request,'You Account Created SuccessFully')
+                  user = form.save()
+                  group = Group.objects.get(name ='Author')
+                  user.groups.add(group)
       else:
             form = userForm()
 
       return render(request, 'blog/signup.html',{'form':form})
 def add_post(request):
       if request.user.is_authenticated:
+            if request.method == 'POST':
+                  form = postForm(request.POST)
+                  if form.is_valid():
+                        form.save()
+                        form = postForm()
+            else:
+                  form = postForm()
            
                   
-            return render(request, 'blog/addpost.html')
+            return render(request, 'blog/addpost.html',{'form':form})
       else:
             return HttpResponseRedirect('/login/')
 
 def update_post(request,id):
       if request.user.is_authenticated:
-            return render(request, 'blog/updatepost.html')
+            if request.method == "POST":
+                  pi= Post.objects.get(pk=id)
+                  pst =postForm(request.POST, instance=pi)
+                  if pst.is_valid():
+                        pst.save()
+                        pst = postForm()
+            else:
+                  pi= Post.objects.get(pk=id)
+                  pst =postForm(instance=pi)
+
+            return render(request, 'blog/updatepost.html',{'form': pst})
       else:
             return HttpResponseRedirect('/login/')
 
 def delete_post(request ,id):
       if request.user.is_authenticated:
-            return HttpResponseRedirect('/dasboard/')
+            if request.method == 'POST':
+                  pst = Post.objects.get(pk=id)
+                  pst.delete()
+            return HttpResponseRedirect('/dashbord/')
       else:
             return HttpResponseRedirect('/login/')
